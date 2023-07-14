5e1d8011d5fea0ec8dd2530358da3b0c2f467c3a
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-07-15 07:41:43 +1000

    Update QuestMain.php

--- src/skyss0fly/Quests/QuestMain.php
+++ src/skyss0fly/Quests/QuestMain.php
@@ -5,4 +5,8 @@ namespace skyss0fly\Quests;
 use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
 use pocketmine\player\Player;
-use pocketmine\plugin\pluginBase;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+
+class QuestMain extends PluginBase implements Listener {
+}
