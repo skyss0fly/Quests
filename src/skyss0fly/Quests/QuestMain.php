5ea8d1fe6e36b10abc4e0fe28bcaddd78e541c6a
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-07-30 18:07:39 +1000

    Update QuestMain.php

--- src/skyss0fly/Quests/QuestMain.php
+++ src/skyss0fly/Quests/QuestMain.php
@@ -3,22 +3,70 @@
 namespace skyss0fly\Quests;
 
 use pocketmine\plugin\PluginBase;
-use pocketmine\event\player\PlayerJoinEvent;
-use pocketmine\event\Listener;
-use pocketmine\scheduler\Task;
+use pocketmine\utils\Config;
 use pocketmine\Player;
-use DateTime;
+use pocketmine\Server;
+use SimpleNPC\NPC;
 
-class Main extends PluginBase implements Listener{
-    private $playerData = [];
-
-    public function onEnable(): void {
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
-        $this->getScheduler()->scheduleRepeatingTask(new TimeUpdateTask($this), 20 * 60); // Update time every minute
-    }
-
-    public function onPlayerJoin(PlayerJoinEvent $event): void {
-        $player = $event->getPlayer();
-        $playerName = $player->getName();
-        $this->playerData[$playerName] = //coming soon
-            }
+class Main extends PluginBase {
+	
+	public function onEnable(){
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		
+		if(!is_dir($this->getDataFolder())){
+			mkdir($this->getDataFolder());
+		}
+		
+		if (!file_exists($this->getDataFolder() . "quests.yml")) {
+			$this->quests = new Config($this->getDataFolder() . "quests.yml", Config::YAML);
+		}
+		
+		$this->getServer()->getPluginManager()->registerEvents(new QuestsListener($this), $this);
+		$this->getLogger()->info("QuestsAPI has been enabled!");
+	}
+	
+	public function getQuests(){
+		return $this->quests->getAll();
+	}
+	
+	public function addQuest(Player $player, $questName, $questDescription, array $rewards){
+		$playerName = $player->getName();
+		
+		$this->quests->set($questName, [
+			"player" => $playerName,
+			"description" => $questDescription,
+			"rewards" => $rewards
+		]);
+		
+		$this->quests->save();
+		
+		$player->sendMessage("You have added a new quest: '$questName'!");
+		
+		$npc = new NPC($player);
+		$npc->setName("Quest NPC: $questName");
+		$npc->showTo($player);
+	}
+	
+	public function completeQuest(Player $player, $questName){
+		$questData = $this->quests->get($questName);
+		if(!$questData){
+			$player->sendMessage("This quest does not exist!");
+			return;
+		}
+		
+		if($questData["player"] !== $player->getName()){
+			$player->sendMessage("You can't complete this quest!");
+			return;
+		}
+		
+		foreach($questData["rewards"] as $reward){
+			Server::getInstance()->dispatchCommand(new ConsoleCommandSender(), "give " . $player->getName() . " " . $reward);
+		}
+		
+		$this->quests->remove($questName);
+		$this->quests->save();
+		
+		$player->sendMessage("You have completed the quest '$questName'!");
+	}
+	
+}
